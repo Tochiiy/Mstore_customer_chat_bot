0eb5d5cf6e7bc0fc1940c0e911cc01493c159aa6
Tochiiy <Tochukwusun24@gmail.com>
2026-06-03 01:41:00 +0300

    Update DB connection Avin hardcoded localhost

--- backend/db/sql_db.py
+++ backend/db/sql_db.py
@@ -5,18 +5,18 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
 def get_db_conn():
     """
-    Establishes and returns a connection to the local MySQL database using credentials from .env file.
+    Establishes and returns a connection to the database using credentials from the environment.
     If the connection fails, it raises an exception with the error details.
     """
     try:
         connection = mysql.connector.connect(
-            host='localhost',
-            user="root",
+            host=os.getenv("DB_HOST"),     
+            user=os.getenv("DB_USER"),      
             password=os.getenv("DB_PASSWORD"),
-            database='shoppingassistantbot'
+            database=os.getenv("DB_NAME"),
+            port=int(os.getenv("DB_PORT", 3306))
         )
         print("[*] Successfully connected to MySQL database.")
         return connection
